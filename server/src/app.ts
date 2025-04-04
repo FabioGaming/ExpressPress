4873f54416ff1f0a513067321503fbae6e493963
FabioGaming <fabio.furch@gmail.com>
2025-04-04 11:41:37 +0200

    added accountRoutes routes
    [ + ] Added routes for account management as well as create the link to express for them

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import accountRoutes from "./routes/accountRoutes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/account", accountRoutes);
 export default app;
