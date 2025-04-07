676373e76e8826c1072bad6f4ba6e8e7bbe6e851
Fabio Gaming <fabio.furch@gmail.com>
2025-04-08 00:42:08 +0200

    Create accountRoutes.test.ts

---
+++ server/src/__TESTS__/controllers/accountRoutes.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import app from "../../app";
+
+describe("Account Routes", () => {
+	describe("GET /api/account/me", () => {
+		it("should return a successful user response", async () => {
+			// TODO
+		});
+
+		it("should fail the user response with an unauthenticated message", async () => {
+			const response = await request(app).get("/api/account/me");
+			expect(response.status).toBe(401);
+			expect(response.body).toHaveProperty("error");
+			expect(response.body).toHaveProperty("code");
+			expect(response.body).toHaveProperty("timestamp");
+		});
+	});
+
+	describe("PUT /api/account/me", () => {
+		it("should accept the changes and return updated user data", async () => {
+			// TODO
+		});
+		it("should fail the request with an unauthenticated message", async () => {
+			const response = await request(app).put("/api/account/me");
+			expect(response.status).toBe(401);
+			expect(response.body).toHaveProperty("error");
+			expect(response.body).toHaveProperty("code");
+			expect(response.body).toHaveProperty("timestamp");
+		});
+		it("should fail the request because the given user slug already exists", async () => {
+			const response = await request(app).put("/api/account/me"); // TODO
+		});
+	});
+});
