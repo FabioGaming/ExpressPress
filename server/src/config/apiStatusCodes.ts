e63ebe3bb6c3dbca6e0ca006aae9a79673f1bdf6
FabioGaming <fabio.furch@gmail.com>
2025-04-10 08:30:18 +0200

    added status code constants

---
+++ server/src/config/apiStatusCodes.ts
@@ -0,0 +1,4 @@
+export const REQUEST_SUCCESS: number = 0;
+export const REQUEST_UNAUTHENTICATED: number = 1;
+export const REQUEST_UNAUTHORIZED: number = 2;
+export const REQUEST_MALFORMED: number = 3;
